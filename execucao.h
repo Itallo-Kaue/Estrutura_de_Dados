6202d863000c8a0e1438d87a8d3d8620b93ccddd
nataliayw03 <nataliay.w@usp.br>
2026-06-06 13:42:04 -0300

    parte 4 adicionada

---
+++ execucao.h
@@ -0,0 +1,26 @@
+#ifndef EXECUCAO_H
+#define EXECUCAO_H
+
+#include <stdbool.h>
+
+typedef struct {
+    double tempo_ms;               
+    long long comparacoes;         
+    long long trocas;              
+    int profundidade_maxima;       
+    long long chamadas_recursivas; 
+    long long memoria_extra_bytes; 
+    bool estavel;                  
+} MetricasExecucao;
+
+
+MetricasExecucao medir_insertionSort(int arr[], int n);
+MetricasExecucao medir_insertion2_0(int arr[], int n);
+MetricasExecucao medir_quickSort(int arr[], int n);
+MetricasExecucao medir_mergeSort(int arr[], int n);
+MetricasExecucao medir_heapSort(int arr[], int n);
+MetricasExecucao medir_shellSort(int arr[], int n);
+
+void imprimir_metricas(const char *nome, MetricasExecucao m);
+
+#endif
